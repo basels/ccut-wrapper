3d54c21a63025b8fc2c5132d755d44194d38bb5e
Basel Shbita <basel921@gmail.com>
2020-06-24 15:05:01 -0700

    annotation element removal finalized & added documentation

--- ui/annotate.py
+++ ui/annotate.py
@@ -1,6 +1,6 @@
-from ccut.main.symbol_map import SymbolMap
-from ccut.main.dimension_map import DimensionMap
 from ccut.main.dimension import DimensionVector
+from ccut.main.dimension_map import DimensionMap
+from ccut.main.symbol_map import SymbolMap
 from fuzzywuzzy.process import extract
 from re import findall
 
@@ -8,18 +8,26 @@ QUDT_NAMESPACE = 'http://www.qudt.org/qudt/owl/1.0.0/unit/Instances.html#'
 URI_SEPARATOR = '#'
 NUM_RESULTS_TO_SUGGEST = 5
 
+# list to hold auto-complete SHEET options
 sheet_flat_list = list()
+# list to hold auto-complete UNIT PREFIX options
 prefix_flat_list = list()
+# list to hold auto-complete UNIT options
 unit_flat_list = list()
+# dictionary mapping from UNIT uri to its QUANTITY KIND (i.e., Gram -> Mass)
 uri_to_qkind = dict()
 
 def init_flat_search_lists(active_dict):
+    ''' Init the auto-complete lists and dictionaries '''
+
     global sheet_flat_list, prefix_flat_list, unit_flat_list, uri_to_qkind
 
+    # Init SHEET options
     for sheet_n in active_dict.keys():
         if sheet_n not in sheet_flat_list:
             sheet_flat_list.append(sheet_n)
 
+    # Init UNIT PREFIX options
     if len(prefix_flat_list) == 0:
         d = SymbolMap.get_instance()
         for _, sym_d in d.si_prefix_map.items():
@@ -29,6 +37,7 @@ def init_flat_search_lists(active_dict):
                 if prefixeless_uri not in prefix_flat_list:
                     prefix_flat_list.append(prefixeless_uri)
 
+    # Init UNIT options and QUANTITY KIND dictionary
     if len(unit_flat_list) == 0:
         d = SymbolMap.get_instance()
         for _, sym_d in d.symbol_map.items():
@@ -39,30 +48,43 @@ def init_flat_search_lists(active_dict):
                     if prefixeless_uri not in unit_flat_list:
                         unit_flat_list.append(prefixeless_uri)
                         if hasattr(op_qu[1], 'quantity_kind'):
-                            uri_to_qkind[prefixeless_uri] = op_qu[1].quantity_kind.rsplit("#")[1]
+                            uri_to_qkind[prefixeless_uri] = op_qu[1].quantity_kind.rsplit(URI_SEPARATOR)[1]
                         
 
 def fuzzy_search_sheet(query):
+    ''' Get top SHEET results matching (fuzzy-search) a given query '''
+
     global sheet_flat_list
-    top_res = extract(query, sheet_flat_list, limit=NUM_RESULTS_TO_SUGGEST)
-    return top_res
+
+    return extract(query, sheet_flat_list, limit=NUM_RESULTS_TO_SUGGEST)
 
 def fuzzy_search_prefix(query):
+    ''' Get top PREFIX results matching (fuzzy-search) a given query '''
+
     global prefix_flat_list
-    top_res = extract(query, prefix_flat_list, limit=NUM_RESULTS_TO_SUGGEST)
-    return top_res
+
+    return extract(query, prefix_flat_list, limit=NUM_RESULTS_TO_SUGGEST)
 
 def fuzzy_search_unit(query):
+    ''' Get top UNIT results matching (fuzzy-search) a given query '''
+
     global unit_flat_list
-    top_res = extract(query, unit_flat_list, limit=NUM_RESULTS_TO_SUGGEST)
-    return top_res
+
+    return extract(query, unit_flat_list, limit=NUM_RESULTS_TO_SUGGEST)
+
+def get_column_row_from_cell_string(cell_string):
+    ''' Return column and row string for a given cell string (i.e., 'A15' -> 'A', '15') '''
+
+    cell_row = findall(r'\d+', cell_string)[0]
+    cell_col = cell_string.split(cell_row)[0].upper()
+
+    return cell_col, cell_row
+
 
 def add_annotation_to_cell(ant_dict, sheet, cell, multiplier, prefix, unit, exponent):    
-    global uri_to_qkind
+    ''' Add a cell annotation to a given dictionary of annotations '''
 
-    cell_row = findall(r'\d+', cell)[0]
-    cell_col = cell.split(cell_row)[0].upper()
-
+    cell_col, cell_row = get_column_row_from_cell_string(cell)
     if sheet not in ant_dict:
         ant_dict[sheet] = dict()
     if cell_col not in ant_dict[sheet]:
@@ -84,10 +106,21 @@ def add_annotation_to_cell(ant_dict, sheet, cell, multiplier, prefix, unit, expo
     else:
         cell_list_inst.append({'parts': [prt_single]})
 
+    
+def update_cell_dimension(ant_dict, sheet, cell):
+    ''' Update the overall dimension of a given cell in a given dictionary of annotations '''
+
+    global uri_to_qkind
+
     dim_vec = DimensionVector()
     d = DimensionMap.get_instance()
-    for a_unt in cell_list_inst[0]['parts']: # assuming a single compound unit in cell
-        unt_name = a_unt['u'].rsplit("#")[1]
+    cell_col, cell_row = get_column_row_from_cell_string(cell)
+
+    cell_dict = ant_dict[sheet][cell_col][cell_row][0] # assuming a single compound unit in cell
+
+    # iterate over each atomic-unit (part) in the list
+    for a_unt in cell_dict['parts']: 
+        unt_name = a_unt['u'].rsplit(URI_SEPARATOR)[1]
         unt_exp = 1
         if 'e' in a_unt:
             unt_exp = a_unt['e']
@@ -97,4 +130,5 @@ def add_annotation_to_cell(ant_dict, sheet, cell, multiplier, prefix, unit, expo
                 dimension = d.qd_map[unt_quantity_kind]
                 if dimension:
                     dim_vec += DimensionVector().set_dimensions(dimension).raise_to_power(unt_exp)
-    cell_list_inst[0]['dimension'] = dim_vec.get_abbr()
+    cell_dict['dimension'] = dim_vec.get_abbr()
+
--- ui/api.py
+++ ui/api.py
@@ -1,51 +1,69 @@
-''' This is the API entry point (and Flask front end) '''
-
-from os import makedirs
-from os.path import exists
-from json import load, dump
-from werkzeug.utils import secure_filename
-from flask import Flask, request, redirect, jsonify, render_template, url_for
-from forms import *
-from annotate import *
+from annotate import init_flat_search_lists, fuzzy_search_sheet, fuzzy_search_prefix, \
+                    fuzzy_search_unit, add_annotation_to_cell, update_cell_dimension
 from ccut import ccut
 from ccut.main.config import Config
 from ccut_sheets import init_globals, process_file
+from flask import Flask, request, redirect, jsonify, render_template, url_for
+from forms import RepresentationForm, TransformationForm, FileUploadForm, AnnotationEditForm
+from json import load, dump
+from os import makedirs
+from os.path import exists
+from werkzeug.utils import secure_filename
 
+# Directory to store temporary work files
 STORAGE_FOLDER = '/tmp/ccut_uploads/'
 
+# Init Flask app
 app = Flask(__name__)
 app.config.from_object(Config)
+# Init ccut sheets processor
 init_globals()
+# Init ccut instance
 ccut = ccut()
+# Init global variables
 g_active_json = dict()
 g_active_filename = ""
 
 @app.route("/")
-def hello():
+def welcome():
+    ''' Page to welcome '''
+
     return render_template('generic.html', data='Welcome to CCUT Wrapper!')
 
 @app.route('/get_canonical_json', methods=['GET', 'POST'])
 def get_canonical_json():
-    if "u" in request.args:
-        unit_string = request.args.get("u")
+    ''' Page to handle Canonical Compound Unit Parsing '''
+
+    ARG_UNIT = 'u'
+    if ARG_UNIT in request.args:
+        unit_string = request.args.get(ARG_UNIT)
         return jsonify(ccut.get_top_ccu(unit_string))
     form = RepresentationForm()
+
     # TODO: show list of all possible representation and choose
     return render_template('ccu_represent.html', form=form)
 
 @app.route('/trans_form', methods=['GET', 'POST'])
 def transform_ccu():
-    if "in_unit" in request.args and "out_unit" in request.args and "in_val" in request.args:
-        unit_in_string = request.args.get("in_unit")
-        unit_out_string = request.args.get("out_unit")
-        val_in = float(request.args.get("in_val"))
+    ''' Page to handle Canonical Compound Unit Transformation '''
+
+    ARG_INUNIT = "in_unit"
+    ARG_OUTUNIT = "out_unit"
+    ARG_INVAL = "in_val"
+    if ARG_INUNIT in request.args and ARG_OUTUNIT in request.args and ARG_INVAL in request.args:
+        unit_in_string = request.args.get(ARG_INUNIT)
+        unit_out_string = request.args.get(ARG_OUTUNIT)
+        val_in = float(request.args.get(ARG_INVAL))
         return jsonify(ccut.convert_str2str(unit_in_string, unit_out_string, val_in))
     form = TransformationForm()
+
     # TODO: show list of all possible representation and choose
     return render_template('ccu_transform.html', form=form)
 
 @app.route('/annotate', methods=['GET', 'POST'])
 def load_annotation_file():
+    ''' Page to handle uploading annotation file (json). Redirects to editor-UI '''
+
     global g_active_json, g_active_filename
 
     form = FileUploadForm()
@@ -64,6 +82,8 @@ def load_annotation_file():
 
 @app.route('/process_tables', methods=['GET', 'POST'])
 def process_spreadsheet_file():
+    ''' Page to handle processing excel file (xlsx). Creates json and redirects to editor-UI '''
+
     global g_active_json, g_active_filename
 
     form = FileUploadForm()
@@ -83,25 +103,25 @@ def process_spreadsheet_file():
     else:
         return render_template('file_upload.html', form=form, file_ext='.xlsx')
 
-@app.route('/search', methods=['GET', 'POST'])
+@app.route('/edit', methods=['GET', 'POST'])
 def edit_annotation_file():
+    ''' Page holding the json editor-UI '''
+
     global g_active_json, g_active_filename
 
     init_flat_search_lists(g_active_json)
     form = AnnotationEditForm()
     if form.validate_on_submit():
-        add_annotation_to_cell(g_active_json,
-                               form.q_sheet.data,
-                               form.cell.data,
-                               form.m.data,
-                               form.q_prefix.data,
-                               form.q_unit.data,
-                               form.e.data)
+        add_annotation_to_cell(g_active_json, form.q_sheet.data, form.cell.data, \
+                               form.m.data, form.q_prefix.data, form.q_unit.data, form.e.data)
+        update_cell_dimension(g_active_json, form.q_sheet.data, form.cell.data)
 
-    return render_template('ccu_search.html', ant_dict=g_active_json, fname=g_active_filename, form=form)
+    return render_template('ccu_json_edit.html', ant_dict=g_active_json, fname=g_active_filename, form=form)
 
 @app.route('/save')
 def save_annotation_file():
+    ''' API to save the json annotation file '''
+
     global g_active_json, g_active_filename
 
     if g_active_filename != "":
@@ -110,29 +130,46 @@ def save_annotation_file():
 
     return redirect(url_for('edit_annotation_file'))
 
-
 @app.route('/remove', methods=['GET'])
 def remove_element_in_annotation_file():
+    ''' API to remove an element (unit-part, cell or sheet) from annotation dictionary '''
+
     global g_active_json
 
-    # s=sheet, c=column, r=row, i=part_index_in_cell
-    if 's' in request.args:
-        sheet = request.args.get('s')
-        if 'c' in request.args:
-            col = request.args.get('c')
-            if 'r' in request.args:
-                row = request.args.get('r')
-                if 'i' in request.args:
-                    idx = request.args.get('i')
+    ARG_SHEET = 's'
+    ARG_COL = 'c'
+    ARG_ROW = 'r'
+    ARG_IDX = 'i' # part index in cell
+    if ARG_SHEET in request.args:
+        sheet = request.args.get(ARG_SHEET)
+        if ARG_COL in request.args:
+            col = request.args.get(ARG_COL)
+            if ARG_ROW in request.args:
+                row = request.args.get(ARG_ROW)
+                if ARG_IDX in request.args:
+                    idx = request.args.get(ARG_IDX)
                     del g_active_json[sheet][col][row][0]['parts'][int(idx)-1]
-                    return redirect(url_for('edit_annotation_file'))
+                    update_cell_dimension(g_active_json, sheet, col+row)
+                    # if there are more unit-parts in cell, return
+                    if len(g_active_json[sheet][col][row][0]['parts']) != 0:
+                        return redirect(url_for('edit_annotation_file'))
+                    # otherwise, continute to remove row (cell)
                 del g_active_json[sheet][col][row]
-                return redirect(url_for('edit_annotation_file'))
+                # if there are no more rows in col, delete col
+                if len(g_active_json[sheet][col]) == 0:
+                    del g_active_json[sheet][col]
+                # if there are more cols in sheet, return
+                if len(g_active_json[sheet]) != 0:
+                    return redirect(url_for('edit_annotation_file'))
+                # otherwise, continute to remove sheet
         del g_active_json[sheet]
+
     return redirect(url_for('edit_annotation_file'))
 
-@app.route("/search/<string:box>")
+@app.route("/edit/<string:box>")
 def process(box):
+    ''' API to get auto-complete suggestions for a given text box '''
+
     query = request.args.get('query')
     suggestions = list()
     if box == 'q_sheet':
@@ -143,9 +180,10 @@ def process(box):
         suggest_list = fuzzy_search_unit(query)
     for itm in suggest_list:
         suggestions.append({'value': itm[0], 'data': itm[0]})
+
     return jsonify({"suggestions":suggestions})
 
-#########################################################################
-
 if __name__ == "__main__":
+    ''' Main/Entry point '''
+
     app.run(host='0.0.0.0', port=5000, debug=True)
